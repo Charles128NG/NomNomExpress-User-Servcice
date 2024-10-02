52f5b62f6c4c4c557d25de69819eba2b4dd4eadc
Aryan <negiaryan53@gmail.com>
2024-10-02 19:27:51 +0530

    first commit

---
+++ src/main/java/com/Negi/NomNomExpress/NomNomExpressApplication.java
@@ -0,0 +1,13 @@
+package com.Negi.NomNomExpress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NomNomExpressApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NomNomExpressApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Negi/NomNomExpress/NomNomExpressApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Negi.NomNomExpress;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NomNomExpressApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
